d1ff7d6fe9cf29998ec6e47c102a0cff2c8381be
Hiten Chadha <69898067+hitenchadha1910@users.noreply.github.com>
2022-07-05 22:23:33 +0200

    Second Upload

--- salary_prediction.py
+++ salary_prediction.py
@@ -6,13 +6,11 @@ dataset = pd.read_csv("Salary Prediction based on Position and Experience.csv")
 X = dataset.iloc[:,[1,2]].values
 y = dataset.iloc[:,-1].values
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor()
 
 regressor.fit(X,y)
-
-
 pickle.dump(regressor, open('model.pkl', 'wb'))
 
 model = pickle.load(open('model.pkl', 'rb'))
-print(model.predict([[1,8]]))
+print(model.predict([[6,8]]))
